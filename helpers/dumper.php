847f4364eee5533dee3143ae64f6902190460753
Andrey Helldar <helldar@ai-rus.com>
2021-04-02 14:09:25 +0300

    Fixed helper

--- helpers/dumper.php
+++ helpers/dumper.php
@@ -4,16 +4,41 @@ use Helldar\LaravelSupport\Facades\Dumper;
 
 if (! function_exists('dd_sql')) {
     /**
-     * Dump the passed variables and end the script.
+     * Throws a message with an SQL query.
      *
      * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
      * @param  bool  $is_short
-     * @param  bool  $is_return
+     */
+    function dd_sql($query, bool $is_short = false): void
+    {
+        Dumper::sqlDd($query, $is_short);
+    }
+}
+
+if (! function_exists('dump_sql')) {
+    /**
+     * Displays a message with an SQL query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
+     * @param  bool  $is_short
+     */
+    function dump_sql($query, bool $is_short = false): void
+    {
+        Dumper::sqlDump($query, $is_short);
+    }
+}
+
+if (! function_exists('sql')) {
+    /**
+     * Displays a message with an SQL query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
+     * @param  bool  $is_short
      *
      * @return array|string
      */
-    function dd_sql($query, bool $is_short = false, bool $is_return = false)
+    function sql($query, bool $is_short = false)
     {
-        return Dumper::ddSql($query, $is_short, $is_return);
+        return Dumper::sql($query, $is_short);
     }
 }
